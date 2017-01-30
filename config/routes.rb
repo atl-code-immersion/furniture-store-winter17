cc9989b53d67826fa78805c7eb02f3cdec359632
Aaron Groch <ar.groch@gmail.com>
2017-01-30 11:29:48 -0500

    in-class work practicing with ActiveRecord calls

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  get 'products' => 'inventory#all_products'
+
+  get 'product' => 'inventory#one_product'
+
+  get 'categorized' => 'inventory#by_category'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
